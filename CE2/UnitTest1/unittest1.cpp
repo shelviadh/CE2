109ce94ce23d5555ee1e477e5328cbb6265f6f7e
andreamarsha <andreamarsha@gmail.com>
2015-09-20 21:54:32 +0800

    unit test for search function

--- CE2/UnitTest1/unittest1.cpp
+++ CE2/UnitTest1/unittest1.cpp
@@ -15,7 +15,6 @@ namespace UnitTest1
 	public:
 		TEST_METHOD(sortTest)
 		{
-			// TODO: Your test code here
 			string inputs[ARRAY_SIZE] = { "table", "apple", "friends", "dog", "car" };
 			string sortedInputs[ARRAY_SIZE] = { "apple", "car", "dog", "friends", "table" };
 
@@ -35,6 +34,28 @@ namespace UnitTest1
 			for (int i = 0; i < processedTexts.size(); i++) {
 				Assert::AreEqual(processedTexts[i], sortedInputs[i]);
 			}
+		}
+
+		TEST_METHOD(searchTest)
+		{
+			string searchForWord = "rains";
+
+			string inputs[ARRAY_SIZE] = { "it rains today", "today is sunny", "bring umbrella when it rains", "tomorrow will rain", "bring coat" };
+			string expectedOutput = "tomorrow will rain";
+			vector<string> hardcodedInput;
+			
+			for (int i = 0; i < ARRAY_SIZE; i++) {
+				hardcodedInput.push_back(inputs[i]);
+			}
+
+			vector<string> actualOutput;
+
+			CommandProcessor cmdProcessor;
+			cmdProcessor.setTextInputs(hardcodedInput);
+			actualOutput = cmdProcessor.searchText(searchForWord);
+
+			Assert::AreEqual(expectedOutput, actualOutput);
+
 		}
 
 	};
